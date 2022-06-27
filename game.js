aa0d0ebd7916e7d00a96afd1ca8c271aabd24f70
wizkas0 <kalle.meurman@hotmail.com>
2022-06-27 11:43:32 +0200

    sdfs

--- game.js
+++ game.js
@@ -7,29 +7,44 @@ class Game {
     }
 
     addPlayer(player2) {
+        //Adds a second player to a game or returns false if there are already two players
         if (!this.players[1]) return false;
         this.players[1] = player2;
     }
 
     choose(playerID, choice){
+        //Sets the choice player with the given id to the given choice, or returns false if it has already been set
         return this.players.filter(player => player.id === playerID)[0].choose(choice);
     }
 
-    getStatus(playerID){
+    getState(playerID){
+        //Returns the state of the game
         const thisPlayer = this.players.filter(player => player.id === playerID)[0];
         const otherPlayer  = this.players.filter(player => !(player.id === playerID))[0];
         const returnObject = {
             "Your choice": thisPlayer.choice || "TBD",
-            "Their choice": "TBD",
+            "Their choice": otherPlayer.choice ? "Hidden" : "TBD",
             "Result": "TBD"
         }
+        if(thisPlayer.choice && otherPlayer.choice) {
+            const result = this.didYouWin(thisPlayer.choice, otherPlayer.choice);
+            returnObject["Their choice"] = otherPlayer.choice;
+            returnObject["Result"] = result;
+        }
+        return returnObject;
     }
 
-    determineWinner(){
-        const possibleChoices = {
-            "Rock": 1,
-            "Paper": 2,
-            "Scissors": 3
+    didYouWin(yourChoice, theirChoice){
+        //Determines whether or not yourChoice beats theirChoice
+        const choiceMap = {
+            "Rock": 0,
+            "Paper": 1,
+            "Scissors": 2
         }
+        if(yourChoice === theirChoice){return "Draw"}
+        else if(
+            (choiceMap[theirChoice] + 1)%3 === choiceMap[yourChoice]
+        ){return "You win!"}
+        else {return "You lose!"}
     }
   }
---
+++ server.js
@@ -0,0 +1,40 @@
+const { v4: uuidv4 } = require('uuid');
+import Game from "game.js";
+import Player from "player.js";
+class Server{
+    games = {}
+    constructor() {
+    }
+
+    createGame(){
+        const uuidGame = uuidv4();
+        const uuidPlayer = uuidv4();
+        const player1 = new Player(uuidPlayer);
+        const game = new Game(uuidGame, player1);
+        games[uuidGame] = game;
+        return {
+            "Your ID": uuidPlayer,
+            "Game URI": "/game/" + uuidGame
+        }
+    }
+
+    joinGame(gameID){
+        const uuidPlayer = uuidv4();
+        const player2 = new Player(uuidPlayer);
+        const game = games[gameID];
+        const added = game.addPlayer(player2);
+        
+        return added ? {"Your ID": uuidPlayer} : false
+    }
+
+    choose(gameID, playerID, choice){
+        const game = games[gameID];
+        const chosen = game.choose(playerID, choice);
+        return chosen ? {"Your choice": choice} : false
+    }
+
+    check(gameID, playerID){
+        const game = games[gameID];
+        return game.getState(playerID);
+    }
+}
