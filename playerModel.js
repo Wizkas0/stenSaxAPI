0139cf2cc325afed4f0c67ac31e1fa4e0ac493bd
wizkas0 <kalle.meurman@hotmail.com>
2022-08-26 17:07:53 +0200

    fixed linter

--- playerModel.js
+++ playerModel.js
@@ -1,13 +1,12 @@
-
 export default class PlayerModel {
-    //The model for a player
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
+  //The model for a player
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
 
-    play(choice) {
-        if (this.choice) throw new Error('You have already chosen!');
-        this.choice = choice;
-    }
+  play(choice) {
+    if (this.choice) throw new Error("You have already chosen!");
+    this.choice = choice;
+  }
 }
